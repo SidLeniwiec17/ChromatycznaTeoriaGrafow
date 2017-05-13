63a63aa23a0e46f10fb665beef73952238786d72
SidLeniwiec <piotrmichalik17@wp.pl>
2017-05-13 14:42:59 +0200

    graph class and loading from txt file

---
+++ CTG/CTG/Content/Graph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTG.Content
+{
+    public class Graph
+    {
+        public int Vertices { get; set; }
+        public List<List<int>> Edges { get; set; }
+        public Graph()
+        {
+
+        }
+        public Graph(int vertices)
+        {
+           Vertices = vertices;
+           Edges = new List<List<int>>();
+           for(int i = 0 ; i < vertices ; i++)
+           {
+               List<int> tmpList = new List<int>();
+               Edges.Add(tmpList);
+           }
+        }
+        public void InitializeGraph(int vertices)
+        {
+            Vertices = vertices;
+            Edges = new List<List<int>>();
+            for (int i = 0; i < vertices; i++)
+            {
+                List<int> tmpList = new List<int>();
+                Edges.Add(tmpList);
+            }
+        }
+    }
+}
---
+++ CTG/CTG/Helpers/InOutTxt.cs
@@ -0,0 +1,100 @@
+﻿using CTG.Content;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CTG.Helpers
+{
+    /// <summary>
+    /// File pattern:
+    /// number_of_vertices
+    /// vertice_index: connection_to_x, connection_to_x2
+    /// vertice_index: connection_to_x3
+    /// vertice_index: connection_to_x4, connection_to_x1
+    /// [...]
+    /// example:
+    /// 5
+    /// 0:1
+    /// 1:2,3
+    /// 2:
+    /// 3:4
+    /// 4:
+    /// </summary>
+    public class InOutTxt
+    {
+        public static Graph LoadGraphFromFile()
+        {
+            Graph graph = new Graph();
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Open graph instance.";
+            open.Filter = "Txt File (*.txt)|*.txt";
+            if (open.ShowDialog() == true)
+            {
+                try
+                {
+                    List<string> lines = new List<string>();
+                    System.IO.StreamReader file =
+                        new System.IO.StreamReader(open.FileName);
+
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                    file.Close();
+                    if (!ParseText(lines, graph))
+                    {
+                        MessageBox.Show("File content has wrong format !");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error ! " + ex.Message);
+                }
+            }
+            return graph;
+        }
+
+        public static bool ParseText(List<string> text, Graph graph)
+        {
+            bool correct = false;
+            int vertices = 0;
+            if (!int.TryParse(text[0], out vertices))
+                return false;
+
+            graph.InitializeGraph(vertices);
+            try
+            {
+                for (int i = 1; i < text.Count; i++)
+                {
+                    string singleLine = text[i].Substring(text[i].IndexOf(":") + 1);
+                    string[] values = singleLine.Split(',');
+                    if (values.Length > 0)
+                    {
+                        for (int q = 0; q < values.Length; q++)
+                        {
+                            if (values[0].Length > 0)
+                            {
+                                int singleEdge = 0;
+                                if (!int.TryParse(values[q], out singleEdge))
+                                    return false;
+                                else
+                                    graph.Edges[i - 1].Add(singleEdge);
+                            }
+                        }
+                    }
+                }
+                correct = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ! " + ex.Message);
+            }
+            return correct;
+        }
+    }
+}
